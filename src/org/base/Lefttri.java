cdc92e8017ba0b1a53d3327e1ce14dc3a1099f1d
Navis@12345* <mercyedvin24@gmail.com>
2021-12-15 16:38:26 +0530

    mercy

---
+++ src/org/base/Lefttri.java
@@ -0,0 +1,14 @@
+package org.base;
+
+public class Lefttri {
+public static void main(String[] args) {
+	int rows = 7;
+	for (int i = rows-1; i>=0; i--) {
+		for (int j = 0; j <=i; j++) {
+			
+			System.out.print("*"+"");
+		}
+		System.out.println();
+	}
+}
+}
